4227a94d813aea9458c9ecab079699c08e993b71
Seong Jin Kim <seongjinkimsjk@gmail.com>
2020-11-10 16:35:07 -0500

    added faker module, created seed.js

---
+++ seed.js
@@ -0,0 +1,37 @@
+const db = require('./server/db')
+const faker = require('faker')
+
+function userMaker() {
+  let userArray = []
+  let random_boolean
+
+  for (let i = 0; i < 100; i++) {
+    random_boolean = Math.random() <= 0.25
+    userArray.push({
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      address: faker.address.streetAddress(),
+      imageUrl: faker.image.people(),
+      isAdmin: random_boolean
+    })
+  }
+  return userArray
+}
+
+const users = userMaker()
+
+function productMaker() {
+  let productArray = []
+  let categoryArray = [clubs, balls]
+  for (let j = 0; j < 100; j++) {
+    productArray.push({
+      name: faker.commerce.productName(),
+      imageUrl: faker.image.technics(),
+      description: faker.commerce.productDescription(),
+      price: faker.commerce.price()
+    })
+  }
+  return productArray
+}
+
+const products = productMaker()
